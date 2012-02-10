180509a51dc31a0b0493fe37a0cfcf295f2379c4
Johan Sveholm <jsveholm@gmail.com>
2012-02-10 16:54:24 +0900

    list forms with words on index page

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe Form do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/words_index/layout_spec.rb
+++ spec/requests/words_index/layout_spec.rb
@@ -63,6 +63,33 @@ describe "Words" do
         div('word',0).div('definition',0).should have_content('1. is an animal')
         div('word',0).div('definition',1).should have_content('2. has a tail')
       end
-    end #layout, with one word, one definition 
+    end #layout, with one word, two definitions
+
+    context "layout, with one word, one definition, one form" do
+      before(:each) do
+        create_word("cat",["is an animal"],["kitten"])
+        visit words_path
+      end
+
+      it "has a div surrounding it" do
+        divs_no('word').should be(1)
+      end
+
+      it "has one definition div" do
+        div('word',0).divs_no('definition').should be(1)
+      end
+
+      it "the definition is diplayed in the div" do
+        div('word',0).div('definition',0).should have_content('1. is an animal')
+      end
+
+      it "has one form div" do
+        div('word',0).divs_no('form').should be(1)
+      end
+
+      it "the form is diplayed in the div" do
+        div('word',0).div('form',0).should have_content('kitten')
+      end
+    end #layout, with one word, one definition, one form
   end
 end
